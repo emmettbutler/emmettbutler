54a846b699f7350bae53be1b0107dc48f386717a
Emmett Butler <emmett.butler321@gmail.com>
2021-09-05 07:34:58 -0700

    updates for ubuntu 20

--- install.sh
+++ install.sh
@@ -30,27 +30,16 @@ git clone https://github.com/nojhan/liquidprompt.git
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+sudo apt-get install tmux ipython vim-gtk-py2 git build-essential wmctrl gimp neovim rkhunter nodejs zlib1g-dev libssl-dev
+pyenv install 3.6.7
+pyenv global 3.6.7
+
 # install a bunch of standard stuff
-sudo apt-get install tmux ipython vim-gtk-py2 git build-essential wmctrl python-pip python3-pip gimp neovim python3-venv rkhunter nodejs
-sudo pip3 install flake8
-sudo pip3 install black
-sudo pip3 install pynvim
-sudo pip3 install neovim
-sudo add-apt-repository ppa:jonathonf/python-3.6
-sudo apt-get update
-sudo apt-get install python-apt python3.6
-cd /usr/lib/python3/dist-packages
-sudo ln -s apt_pkg.cpython-{35m,36m}-x86_64-linux-gnu.so
+pip install flake8
+pip install black
 
 sudo npm install -g js-beautify
 
-git clone git@github.com:edenhill/librdkafka.git
-cd librdkafka
-./configure && make && sudo make install && sudo ldconfig
-
-
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-
 git clone git@github.com:Parsely/engineering.git ~/git/parsely/engineering
-
