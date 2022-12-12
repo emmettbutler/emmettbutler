b803278534a711a9fbcee14912930a3942e2731c
Emmett Butler <emmett.butler321@gmail.com>
2022-12-12 10:03:18 -0800

    update flake8-isort and locks

--- nixos/nvimeb/nix/flake8-isort.nix
+++ nixos/nvimeb/nix/flake8-isort.nix
@@ -2,10 +2,10 @@
 
 buildPythonPackage rec {
   pname = "flake8-isort";
-  version = "4.2.0";
+  version = "5.0.3";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "JlcVAM1Ul2u8DPEAb/vNGmjdEC+Ba3oQUbIZYWup/uA=";
+    sha256 = "CVE5jDQ8Z/STNAetu/tJXU33wDhlDF0FdToAbvz+s5A=";
   };
   checkInputs = with python3Packages; [ flake8 isort pytest ];
 }
