e83139feb28d34fff2d5b0359805318598e89c60
Emmett Butler <emmett.butler321@gmail.com>
2022-12-20 17:55:35 -0800

    add python linters and ls

--- nixos/nvimeb/init.lua
+++ nixos/nvimeb/init.lua
@@ -293,7 +293,7 @@ end
 local servers = { 'jedi_language_server', 'bashls', 'terraformls' }
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 -- Setup language servers
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
