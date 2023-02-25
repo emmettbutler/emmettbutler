5edc21b3318e58a318189338a44dc2ed2b29b3ef
Emmett Butler <emmett.butler321@gmail.com>
2023-02-25 10:05:23 -0800

    upgrade to nixos 22.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     neovimeb.url = "path:/home/emmett/git/emmettbutler/nixos/nvimeb";
   };
