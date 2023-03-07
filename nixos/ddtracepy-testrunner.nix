d9830afd74b8a11237a8e3bf9edcc9444b15f127
Emmett Butler <emmett.butler321@gmail.com>
2023-03-06 17:08:03 -0800

    commit testrunner shell for dd-trace-py

---
+++ nixos/ddtracepy-testrunner.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.python310
+    pkgs.python310Packages.pip
+    pkgs.python310Packages.virtualenv
+    pkgs.gcc
+  ];
+  shellHook = ''
+    python -m virtualenv .venv
+    source .venv/bin/activate
+    sudo .venv/bin/pip install Cython
+    sudo .venv/bin/pip install -e .
+    sudo .venv/bin/python setup.py develop
+    if [[ -f ../riot/setup.py ]]; then
+        echo "Using local riot"
+        PREV_DIR=`pwd`
+        cd ../riot
+        sudo $PREV_DIR/.venv/bin/python setup.py develop
+        cd $PREV_DIR
+    else
+        echo "Using riot from pypi"
+        sudo .venv/bin/pip install riot
+    fi
+  '';
+}
+
