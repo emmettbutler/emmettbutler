053e2dc1606d0a3980ff7f103f74e62a2c5906ba
Emmett Butler <emmett.butler321@gmail.com>
2023-03-11 09:58:12 -0800

    use a custom Black derivation that installs a version of Black old enough to support 2.7 code formatting
    the process for making it was basically copypasting the latest Black derivation from here https://github.com/NixOS/nixpkgs/blob/aa5744a532134a417dc49e7b4f724804cf1bec67/pkgs/development/python-modules/black/default.nix
    changing the installed Black version to the latest that supports 2.7 targeting
    and then fixing nix build errors by adding dependencies from the old derivation that used a similar version https://github.com/NixOS/nixpkgs/blob/0e7ee4d6a05201a08944f81c33337a72b84fa5d0/pkgs/development/python-modules/black/default.nix
    it also involved making a custom click derivation that used an old version compatible with the Black version in question
    
    jedi-language-server was causing a conflict with this old click version, and i dont really know what it does, so i removed it for now. maybe ill get it back when this config doesnt have to support py27 coding anymore

--- nixos/nvimeb/nix/black.nix
+++ nixos/nvimeb/nix/black.nix
@@ -1,33 +1,79 @@
-{ buildPythonPackage, fetchPypi, python3Packages, myclick }:
+{ stdenv, lib, buildPythonPackage, dataclasses, fetchPypi, pythonOlder
+, pytestCheckHook, aiohttp, aiohttp-cors, appdirs, click804, colorama
+, hatch-fancy-pypi-readme, hatch-vcs, hatchling, mypy-extensions, pathspec
+, parameterized, platformdirs, regex, toml, tomli, typed-ast, typing-extensions
+, uvloop }:
 
 buildPythonPackage rec {
   pname = "black";
   version = "21.4b2";
-  #version = "19.10b0";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "/JvPO0grBcHzX2qILAedwBucd5WCdTL0zEPA7IgGe7w=";
-    #sha256 = "wu23Ogjp4Ob2Wg5q8YsFm4sc3VvvmX16Cxgd+T3IFTk=";
+    hash = "sha256-/JvPO0grBcHzX2qILAedwBucd5WCdTL0zEPA7IgGe7w=";
   };
-  checkInputs = with python3Packages; [
-    setuptools_scm
-    regex
-    pathspec
-    mypy-extensions
-    toml
-    appdirs
-    myclick
-    typed-ast
-    attrs
+
+  nativeBuildInputs = [ hatch-fancy-pypi-readme hatch-vcs hatchling ];
+
+  # Necessary for the tests to pass on Darwin with sandbox enabled.
+  # Black starts a local server and needs to bind a local address.
+  __darwinAllowLocalNetworking = true;
+
+  nativeCheckInputs = [ pytestCheckHook parameterized aiohttp ];
+
+  preCheck = ''
+    export PATH="$PATH:$out/bin"
+
+    # The top directory /build matches black's DEFAULT_EXCLUDE regex.
+    # Make /build the project root for black tests to avoid excluding files.
+    touch ../.git
+  '' + lib.optionalString stdenv.isDarwin ''
+    # Work around https://github.com/psf/black/issues/2105
+    export TMPDIR="/tmp"
+  '';
+
+  disabledTests = [
+    # requires network access
+    "test_gen_check_output"
+  ] ++ lib.optionals stdenv.isDarwin [
+    # fails on darwin
+    "test_expression_diff"
+    # Fail on Hydra, see https://github.com/NixOS/nixpkgs/pull/130785
+    "test_bpo_2142_workaround"
+    "test_skip_magic_trailing_comma"
+  ];
+  # multiple tests exceed max open files on hydra builders
+  doCheck = !(stdenv.isLinux && stdenv.isAarch64);
+
+  propagatedBuildInputs = [
     aiohttp
     aiohttp-cors
-    pytest
-    pytest-mock
-    pytest-cases
-    coverage
-    tomli
-    typing-extensions
-    packaging
+    appdirs
+    click804
+    mypy-extensions
+    pathspec
     platformdirs
-  ];
+    regex
+    toml
+    tomli
+  ] ++ lib.optional (pythonOlder "3.7") dataclasses
+    ++ lib.optional (pythonOlder "3.8") typed-ast
+    ++ lib.optional (pythonOlder "3.10") typing-extensions;
+
+  passthru.optional-dependencies = {
+    d = [ aiohttp aiohttp-cors ];
+    colorama = [ colorama ];
+    uvloop = [ uvloop ];
+  };
+
+  meta = with lib; {
+    description = "The uncompromising Python code formatter";
+    homepage = "https://github.com/psf/black";
+    changelog = "https://github.com/psf/black/blob/${version}/CHANGES.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [ sveitser autophagy ];
+  };
 }
--- nixos/nvimeb/nix/mypython.nix
+++ nixos/nvimeb/nix/mypython.nix
@@ -3,7 +3,6 @@
 let
   my-python-packages = python-packages:
     with python-packages; [
-      jedi-language-server
       flake8
       flake8-isort
       isort
