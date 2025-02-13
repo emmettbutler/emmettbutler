e2663c2b36db0963bc0bb71af0bf2ede46618428
Emmett Butler <emmett.butler321@gmail.com>
2025-02-12 17:28:29 -0800

    fingerprint auth is simpler now

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -274,29 +274,7 @@ with lib;
     xkb.variant = "";
   };
   services.gnome.gnome-online-accounts.enable = lib.mkForce false;
-  services.gnome.gnome-keyring.enable = lib.mkForce false;
-
-  security.pam.services.login.fprintAuth = false;
-  security.pam.services.gdm-fingerprint =
-    lib.mkIf (config.services.fprintd.enable) {
-      text = ''
-        auth       required                    pam_shells.so
-        auth       requisite                   pam_nologin.so
-        auth       requisite                   pam_faillock.so      preauth
-        auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
-        auth       optional                    pam_permit.so
-        auth       required                    pam_env.so
-        auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
-        auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
-
-        account    include                     login
-
-        password   required                    pam_deny.so
-
-        session    include                     login
-        session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-      '';
-    };
+  services.gnome.gnome-keyring.enable = lib.mkForce true;
 
   virtualisation.docker.enable = true;
 
