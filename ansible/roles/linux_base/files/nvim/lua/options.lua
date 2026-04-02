63c5b9243a769c48f9e01b68ce81ed9f4f669a9e
Emmett Butler <emmett.butler321@gmail.com>
2026-04-02 09:00:42 -0700

    set some preferred vim options

--- ansible/roles/linux_base/files/nvim/lua/options.lua
+++ ansible/roles/linux_base/files/nvim/lua/options.lua
@@ -3,13 +3,23 @@ require("nvchad.options")
 local o = vim.opt
 
 -- Your custom options
-vim.o.spell = true
-vim.o.termguicolors = true
-vim.o.wildmenu = true
-vim.o.wildmode = "longest,list"
+o.autoindent = true
+o.expandtab = true
+o.history = 1000
+o.listchars = "tab:>-,trail:~,extends:>,precedes:<"
+o.relativenumber = true
+o.shiftwidth = 0
+o.shiftround = true
+o.spell = true
+o.tabstop = 4
+o.termguicolors = true
+o.wildmenu = true
+o.wildmode = "longest,list"
+o.wrap = false
 
+-- %% expands to current directory path
 vim.cmd([[
-cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'
+  cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'
 ]])
 
 -- Line numbers (must use vim.opt to override NvChad defaults)
