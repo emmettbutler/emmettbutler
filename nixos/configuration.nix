873745abfc9e46aaacb8af2b796038d663ddbf33
Emmett Butler <emmett.butler321@gmail.com>
2026-05-30 07:57:27 -0700

    enable resolved to support vpn service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,6 +121,16 @@ with lib;
     pulse.enable = true;
     jack.enable = true;
   };
+  networking.nameservers =
+    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
+  };
   systemd = {
     services = {
       vpn = {
